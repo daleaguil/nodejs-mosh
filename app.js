20c3f9822c229de76a04c59922047decd467e835
Dale <dale@codersguild.net>
2021-07-14 01:55:01 +0800

    initial commit

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+const http = require('http');
 
-const Logger = require('./logger');
-const logger = new Logger();
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Hello World');
+        res.end();
+    }
 
-//Register a listener
-logger.on('messageLogged', (e) => {
-    console.log('Listener called', e);
-}); 
+    if (req.url === '/api/courses') {
+        res.write(JSON.stringify([1, 2, 3]));
+        res.end();
+    }
+});
 
-logger.log('test');
+server.listen(3000);
+
+console.log('Listening on port 3000...');
